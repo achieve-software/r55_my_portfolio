be3ed0dad6737af8931ae9f967bf04ed3906f32f
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-24 20:57:20 +0300

    NAVBAR

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
- 
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
 function App() {
-  return (
+  return(
     <div className="App">
-     
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/products" element={<Projects/>} />
+          <Route path="/products" element={<Contact/>} />
+           
+          
+        </Routes>
+       
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {Link, NavLink} from 'react-router-dom';
+  
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+      <header>
+        <div className='container'>
+          <div className='d-flex justify-content-between align-items-center'>
+  
+            <div className='logo'>
+              <Link to='/' className='brand'>CV</Link>
+            </div>
+            <nav className='nav nav-pills'>
+              <NavLink to='/' 
+                className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
+              >Home</NavLink>
+              <NavLink to='about'
+                className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
+              >About</NavLink>
+              <NavLink to='projects'
+                className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
+              >Projects</NavLink>
+               <NavLink to='contact'
+                className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
+              >Contact</NavLink>
+            </nav>
+  
+          </div>
+        </div>
+      </header>
+    )}
 
-export default Navbar
+    export default Navbar
