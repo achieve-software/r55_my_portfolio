7903be0120d332b3d940ce26b6945a4a6ab68621
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-30 11:31:22 +0300

    ss

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Projects = () => {
+import "./Projects.scss";
+import { imageData } from "../../helper/imageData.js";
+import { Link } from "react-router-dom";const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
-
-export default Projects
+    <div className="projects-container">
+      <div className="title-container">
+        <h1 className="title">My Projects</h1>
+      </div>
+      <div className="projects-items">
+        {imageData.map((item, i) => (
+          <Link key={i + 1} to={item.link} target="_blank" className="project-navigator">
+            <div className="projects-item">
+              <img
+                className="project-image"
+                src={item.image}
+                alt="My Project"
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};export default Projects;
